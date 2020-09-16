f8ef0a15b39aab09eaace0312dfe15f1990a2cc3
UrielCh <uriel.chemouni@gmail.com>
2020-09-16 11:35:19 +0300

    remove console.logs

--- api/src/sample.ts
+++ api/src/sample.ts
@@ -3,15 +3,20 @@ import ProxmoxEngine from "./ProxmoxEngine";
 
 async function test() {
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-    const auth = await import('../../../auth') as any;
+    const auth = await import('../../../auth');
     const password = auth.default.pass;
-    // console.log(process.env)
+    // connect to proxmox
     const promox = proxyProxmox(new ProxmoxEngine({host: '10.0.0.95', password}));
+    // liste nodes
     const nodes = await promox.nodes.$get();
+    // iterate cluster nodes
     for (const node of nodes) {
         const theNode = promox.nodes.$(node.node);
+        // list Qemu VMS
         const qemus = await theNode.qemu.$get({full:true});
+        // iterate Qemu VMS
         for (const qemu of qemus) {
+            // do some suff.
             const config = await theNode.qemu.$(qemu.vmid).config.$get();
             console.log(`vm: ${config.name}, memory: ${config.memory}`);
             // const vnc = await theNode.qemu.$(qemu.vmid).vncproxy.$post();
@@ -23,4 +28,3 @@ async function test() {
 }
 
 test().catch(console.error);
-// console.log(process.env)
