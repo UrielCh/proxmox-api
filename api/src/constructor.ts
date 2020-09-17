de8f7994ae83bb3381482cace632977dc6c47f53
UrielCh <uriel.chemouni@gmail.com>
2020-09-17 19:40:28 +0300

    clean

--- api/src/constructor.ts
+++ api/src/constructor.ts
@@ -2,10 +2,6 @@ import { Proxmox } from "./model";
 import ProxmoxEngine from "./ProxmoxEngine";
 import { buildApiProxy } from "./proxy";
 
-//export function proxyProxmox(ovhEngine: ApiRequestable): Proxmox.Api {
-//    return buildApiProxy(ovhEngine, '/api2/json');
-//}
-
 export function proxmoxApi(options: { host: string, port?: number, schema?: 'https' | 'http', username?: string, password: string }): Proxmox.Api {
     return buildApiProxy(new ProxmoxEngine(options), '/api2/json');
 }
