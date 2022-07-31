9a1e88a2db66b45b283780e7417c59bc216aad95
Muhammad Ali Fahri <saya@fahri.dev>
2022-08-01 01:56:34 +0700

    feat: check doDequest to validate is ProxmoxEngine

--- api/src/constructor.ts
+++ api/src/constructor.ts
@@ -1,13 +1,17 @@
 import { Proxmox } from "./model";
 import ProxmoxEngine from "./ProxmoxEngine";
-import { buildApiProxy } from "./proxy";
+import { ApiRequestable, buildApiProxy } from "./proxy";
 import { ProxmoxEngineOptions } from './ProxmoxEngine';
 
-export function proxmoxApi(options: ProxmoxEngineOptions | ProxmoxEngine): Proxmox.Api {
-    if ((options as any).getTicket)
-        return buildApiProxy(options as ProxmoxEngine, '/api2/json');
+function isProxmoxEngine(val: any): val is ApiRequestable {
+    return 'doRequest' in val;
+}
+
+export function proxmoxApi(options: ProxmoxEngineOptions | ApiRequestable): Proxmox.Api {
+    if (isProxmoxEngine(options))
+        return buildApiProxy(options, '/api2/json');
     else
-        return buildApiProxy(new ProxmoxEngine(options as ProxmoxEngineOptions), '/api2/json');
+        return buildApiProxy(new ProxmoxEngine(options), '/api2/json');
 }
 
 export default proxmoxApi;
