ec057ca4a89087c6dad8922dbaf2c6a77824aa44
UrielCh <uriel.chemouni@gmail.com>
2022-08-02 20:44:02 +0300

    update generator

--- generator/src/gen.ts
+++ generator/src/gen.ts
@@ -55,6 +55,22 @@ export class Generator {
     usedNamed: { [name: string]: string } = {};
 
     getFinalData(): string {
+        const licence = `// Proxmox-API Interactive proxmox API for developpers how do not like reading docs
+// Copyright (C) 2020-2022  Chemouni Uriel <uchemouni@gmail.com>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+`
         const header = 'export namespace Proxmox {';
         const typings = this.getNewType().join(EOL)
         const code = this.code.join(EOL);
@@ -62,7 +78,7 @@ export class Generator {
         const retTypes = this.getretTypes().join(EOL);
         const footer = `}${EOL}export default Proxmox;`;
 
-        return header + EOL + typings + EOL + retTypes + EOL + code + EOL + footer + EOL;
+        return licence + EOL + header + EOL + typings + EOL + retTypes + EOL + code + EOL + footer + EOL;
     }
 
     appendComment(dest: string[], lineOffset: string, ...lines: string[]) {
